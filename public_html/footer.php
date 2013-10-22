66f588b0a31bc0c70ce54a7d3706d9091c385b71
Melvin Ramos <melvin.ramos1991@gmail.com>
2013-10-22 12:50:00 -0430

    pagina version beta decente

---
+++ public_html/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>&copy; Copyright Red de Gen&eacutetica para la Conservaci&oacuten. Dise&ntildeo por <a href="http://www.turpialdevelopment.com">Turpial Development</a>. 2013</p>
+</footer>
---
+++ public_html/informacionVisitante.php
@@ -0,0 +1,95 @@
+<?php $thisPage="becas"; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>CLGC - Informaci&oacuten Visitante</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/flexslider.css">
+        
+        <script src="js/vendor/jquery-1.8.0.min.js" type="text/javascript"></script>  
+        <!--[if lt IE 9]>
+          <script src="js/modernizr.custom.js"></script>
+        <![endif]-->
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <script src="js/vendor/jquery.flexslider-min.js" type="text/javascript"></script>
+        <script src="js/vendor/functions.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+    <?php include 'header.php'; ?>
+
+    <?php include 'jumbotron.php'; ?>
+
+    <div id="main" class="container">
+      <div id="info" class="testimonial">
+        <h2>Transporte a&eacutereo</h2>
+        <p>El Aeropuerto Internacional de Maiquet&iacutea (IAIM) ser&aacute el terminal de llegada para los participantes internacionales que utilicen transporte a&eacutereo.</p>
+        <p><b>Algunas aerol&iacuteneas internacionales:</b></p>
+        <ul>
+          <li><a href="http://www.aerolineas.com.ar">Aerol&iacuteneas Argentinas</a></li>
+          <li><a href="http://www.aeromexico.com.mx/">Aeromexico</a></li>
+          <li><a href="http://www.aa.com/homePage.do">American Airlines</a></li>
+          <li><a href="http://www.avianca.com">Avianca</a></li>
+          <li><a href="http://www.caribbean-airlines.com/">Caribbean Airlines</a></li>
+          <li><a href="http://www.united.com/web/">Continental Airlines</a></li>
+          <li><a href="http://www.copaair.com/">Copa Airlines</a></li>
+          <li><a href="http://www.cubana.cu/home/">Cubana de Aviaci&oacuten</a></li>
+          <li><a href="http://es.delta.com/">DELTA Airlines</a></li>
+          <li><a href="http://www.lan.com/">LAN Chile</a></li>
+          <li><a href="http://www.taca.com/">TACA/LACSA</a></li>
+          <li><a href="http://www.tam.com.br/">TAM Airlines</a></li>
+        </ul>
+        <p>Para todas las salidas internacionales ¡La tasa aeroportuaria ya est&aacute inclu&iacuteda en su boleto!</p>
+        <h2>Visas y vacunas</h2>
+        <p>Por favor, visite este enlace para <a href="http://www.mre.gov.ve/index.php?option=com_content&view=article&id=2143&Itemid=194">Informaci&oacuten sobre Visas</a> mientras prepara su viaje para Venezuela. Igualmente consulte si desde su pa&iacutes es necesario contar con vacunas como fiebre amarilla.</p>
+        <h2>Hoteler&iacutea</h2>
+        <p>A continuaci&oacuten encontrar&aacuten una breve lista de hoteles cercanos a la sede del Congreso. Pr&oacuteximamente proporcionaremos m&aacutes informaci&oacuten sobre alternativas de alojamiento.</p>
+        <ul>
+          <li><a href="http://www.hotelolecaribe.com/">Hotel Ol&eacute Caribe (sede del ICLGC)</a></li>
+          <li><a href="http://lasquinceletras.net/intro/">Hotel Las Quince Letras</a></li>
+          <li><a href="http://www.venezuelatuya.com/hoteles/">Hostal Colina Real</a></li>
+          <li><a href="http://www.venezuelatuya.com/hoteles/">Hotel y Restarurant La Posada del Hidalgo</a></li>
+        </ul>
+        <h2>Clima</h2>
+        <p>La sede del I CLGC est&aacute ubicada en el <a href="http://es.wikipedia.org/wiki/Vargas">estado Vargas</a>, el cual comprende parte de la regi&oacuten costera del norte de Venezuela. Su clima es generalmente de tipo tropical h&uacutemedo, con una temperatura promedio anual de 28ºC. Es una zona de playas caribeñas, por lo que le invitamos a traer su traje de baño, sombrero y protector solar en caso de planear tener un d&iacutea de playa. Adicionalmente, una pieza de abrigo ligero ser&iacutea una buena recomendaci&oacuten si decidiera visitar <a href="http://es.wikipedia.org/wiki/Galip%C3%A1n">Galip&aacuten</a> y/o el <a href="http://es.wikipedia.org/wiki/Parque_Nacional_El_%C3%81vila">Parque Nacional Waraira Repano</a>, o bien si no est&aacute acostumbrado a &aacutereas con aire acondicionado. Antes de viajar, le recomendamos consultar el pron&oacutestico del tiempo para la zona del Estado Vargas <a href="http://www.meteored.com.ve/tiempo-en_La+Guaira-America+Sur-Venezuela-Distrito+Federal--1-23810.html">aqu&iacute</a>.</p>
+        <h2></h2>
+        <ul>
+          <li>Las playas del litoral central, que entre las m&aacutes conocidas est&aacuten: Playa Grande, Catia La Mar, Macuto, <a href="http://es.wikipedia.org/wiki/Caraballeda">Caraballeda</a>, El Palmar, Naiguat&aacute, Anare, Camur&iacute Chico, Los Caracas, Los Cocos, Playa verde, Los &aacutengeles, entre muchas otras.</li>
+          <li>El <a href="http://es.wikipedia.org/wiki/Parque_Nacional_El_%C3%81vila">Parque Nacional Waraira Repano</a>, que abarca la Cordillera de la Costa, una larga cadena de montañas que se extiende a lo largo de las costas de Venezuela. Para informaci&oacuten sobre ´c&oacutemo visitar el parque vaya a este <a href="http://www.el-avila.com/">enlace</a>.</li>
+          <li>El poblado de <a href="http://es.wikipedia.org/wiki/Galip%C3%A1n">Galip&aacuten</a>, ubicado en la ladera norte del cerro el &aacutevila. Para mayor informaci&oacuten sobre c&oacutemo llegar, cu&aacuteles lugares visitar y opciones de alojamiento vaya a este <a href="http://www.galipan.net/apps/index.php">enlace</a>.</li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <?php include 'footer.php'; ?>
+    </div> <!-- /container -->
+
+    <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>-->
+    
+    <!--<script src="js/vendor/jquery-1.10.1.min.js"></script>-->
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <script src="js/main.js"></script>
+
+    <!--<script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src='//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>-->
+    </body>
+</html>
