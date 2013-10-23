74115cc675bde53f79e984329c8669aa5542ceee
Melvin Ramos <melvin.ramos1991@gmail.com>
2013-10-23 09:44:58 -0430

    Primer commit

---
+++ public_html/programa.php
@@ -0,0 +1,137 @@
+<?php $thisPage="programa"; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>CLGC - Programa</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/flexslider.css">
+        
+        <script src="js/vendor/jquery-1.8.0.min.js" type="text/javascript"></script>  
+        <!--[if lt IE 9]>
+          <script src="js/modernizr.custom.js"></script>
+        <![endif]-->
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <script src="js/vendor/jquery.flexslider-min.js" type="text/javascript"></script>
+        <script src="js/vendor/functions.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+    <?php include 'header.php'; ?>
+
+    <?php include 'jumbotron.php'; ?>
+
+    <div id="main" class="container">
+      
+      <table id="infoTable">
+        <thead>
+        <tr>
+          <th>D&iacutea</th>
+          <th>Ma&ntildeana</th>
+          <th>Tarde</th>
+          <th>Noche</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Lunes, 5/5</td>
+          <td>
+            <p>Registro</p>
+            <p>Cursos pre-congreso:
+“Introducci&oacuten a la gen&eacutetica para la conservaci&oacuten” y “Diseño de proyectos de gen&eacutetica para la conservaci&oacuten”</p>
+          </td>
+          <td>
+            <p>Bienvenida</p>
+            <p>Conferencia Antonio Sol&eacute Cava</p>
+          </td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>Martes, 6/5</td>
+          <td>Presentaciones orales</td>
+          <td>
+            <p>Conferencia</p>
+            <p>Presentaciones orales</p>
+          </td>
+          <td>Carteles</td>
+        </tr>
+        <tr>
+          <td>Mi&eacutercoles, 7/5</td>
+          <td>Presentaciones orales</td>
+          <td>
+            <p>Conferencia</p>
+            <p>Presentaciones orales</p>
+          </td>
+          <td>Mesa redonda (miembros ReGeneC)</td>
+        </tr>
+        <tr>
+          <td>Jueves, 8/5</td>
+          <td>
+            <p>Presentaciones orales</p>
+            <p>Simposio Cacao</p>
+          </td>
+          <td>
+            <p>Conferencia</p>
+            <p>Presentaciones orales</p>
+          </td>
+          <td>Carteles</td>
+        </tr>
+        <tr>
+          <td>Viernes, 9/5</td>
+          <td>
+            <p>Presentaciones orales</p>
+            <p>Foro &Eacutetica y Conservaci&oacuten</p>
+          </td>
+          <td>
+            <p>Conferencia</p>
+             <p>Clausura</p>
+            <p>Curso “Secuenciaci&oacuten de pr&oacutexima generaci&oacuten y su uso en la gen&eacutetica para la conservaci&oacuten”</p>
+          </td>
+          <td>-</td>
+        </tr>
+        </tbody>
+      </table>
+
+      <div id="info" class="container">
+        <h2>Entre los conferencistas tendremos:</h2>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;- Dr. Jonathan Ballou, Investigador del Center for Conservation and Evolutive Genetics.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Smithsonian Conservation Biology Institute, USA.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;- Dr. Antonio Solé-Cava, Laboratório de Biodiversidade Molecular.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Departamento de Genética, Instituto de Biologia, Universidade Federal do Rio de Janeiro, Brasil.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;- Dra. Cristina Yumi Miyaki, Laboratorio de Genética e Evolução Molecular de Aves.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Departamento de Biologia, Instituto de Biociências, Universidade de São Paulo, Brasil.</p>
+      </div>
+
+      <hr>
+
+      <?php include 'footer.php'; ?>
+    </div> <!-- /container -->
+
+
+    <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>-->
+    
+    <!--<script src="js/vendor/jquery-1.10.1.min.js"></script>-->
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <script src="js/main.js"></script>
+
+    <!--<script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src='//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>-->
+    </body>
+</html>
